a6d7eed63e74d2950a27c340c3b5cabc194470c8
AuroraCoders <shehnazhaider3@gmail.com>
2026-05-26 00:02:34 +0500

    Update

--- export_data.py
+++ export_data.py
@@ -11,7 +11,7 @@ project = hopsworks.login(api_key_value=os.getenv("HOPSWORKS_API_KEY"))
 fs = project.get_feature_store()
 
 # Load your feature group
-feature_group = fs.get_feature_group(name="aqi_prediction", version=1)
+feature_group = fs.get_feature_group(name="skardu_aqi_prediction_v3", version=3)
 
 # Read data into pandas DataFrame
 df = feature_group.read()
